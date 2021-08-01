f65e16cf13c804b4b1b862b690e4df41e272c43a
Dipityli <40866259+dipityli@users.noreply.github.com>
2021-08-01 21:52:00 +0530

    Removed unused code

--- app.py
+++ app.py
@@ -43,18 +43,6 @@ def submit():
 def about():
     return render_template('about-us.html') 
 
-# @app.route('/predict')
-# def predict():
-#     return render_template('predict.html')
-    
-# @app.route('/predict_form_inputs', methods=['POST'])
-# def predict_form_inputs():
-#     return render_template('predict.html', output=output)
-
-# @app.route('/privacy-policy')
-# def privacy_policy():
-#     return render_template('privacy-policy.html')
-
 def errorhandler(e):
     if not isinstance(e, HTTPException):
         e = InternalServerError()
