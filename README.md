556a38b67a983ba14157eb480f73646bdfd22dff
dipityli <40866259+dipityli@users.noreply.github.com>
2021-08-01 17:34:35 +0530

    redesigned the entire website

--- README.md
+++
@@ -1,9 +0,0 @@
-# LiteratureLibre
-LiteratureLibre is an open source library of written stories, poems and literature!
-https://literaturelibre.cobweb-aclevo.org
-
-# Made with love by Cob:web-Aclevo and our Open source contributors:
-
-Adam Salt - Cob:web-Aclevo Director & Website maintainer
-
-### Please join the Cob:web-Aclevo discord to talk to us and contribute to our projects: https://discord.gg/z3GXvGtuek
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
+@app.route('/about')
+def about():
+    return render_template('about-us.html') 
+
+# @app.route('/predict')
+# def predict():
+#     return render_template('predict.html')
+    
+# @app.route('/predict_form_inputs', methods=['POST'])
+# def predict_form_inputs():
+#     return render_template('predict.html', output=output)
+
+# @app.route('/privacy-policy')
+# def privacy_policy():
+#     return render_template('privacy-policy.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
